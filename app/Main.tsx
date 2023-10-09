cdf985d969c02b5d0d51e597ad9d99e05dee89c6
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-10-09 16:19:28 -0600

    changing some content from template

--- app/Main.tsx
+++ app/Main.tsx
@@ -12,10 +12,13 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
+          </h1>
+          <p className="text-2xl leading-7 text-gray-800 dark:text-gray-400">
+            Navigating the Web Developer's Odyssey
+          </p>
+          <p className="text-sm leading-7 text-gray-500 dark:text-gray-400">
+            by {siteMetadata.author}
           </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
