8701a04fd189943e318f5c8f877bb4de30d5275b
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-10-23 17:06:56 +0200

    homepage layout

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,86 +3,63 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 
-const MAX_DISPLAY = 5
+const MAX_DISPLAY = 6
 
 export default function Home({ posts }) {
   return (
-    <>
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            {siteMetadata.description}
-          </h1>
-          <p className="text-2xl leading-7 text-gray-800 dark:text-gray-400">
-            Exploring the Junior Web Developer's Path
-          </p>
-          <p className="text-sm leading-7 text-gray-500 dark:text-gray-400">
-            by {siteMetadata.author}
-          </p>
-        </div>
-        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-          {!posts.length && 'No posts found.'}
-          {posts.slice(0, MAX_DISPLAY).map((post) => {
-            const { slug, date, title, summary, tags } = post
-            return (
-              <li key={slug} className="py-12">
-                <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    <dl>
-                      <dt className="sr-only">Published on</dt>
-                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
-                      </dd>
-                    </dl>
-                    <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-6">
-                        <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                            <Link
-                              href={`/blog/${slug}`}
-                              className="text-gray-900 dark:text-gray-100"
-                            >
-                              {title}
-                            </Link>
-                          </h2>
-                          <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
-                          </div>
-                        </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                          {summary}
-                        </div>
-                      </div>
-                      <div className="text-base font-medium leading-6">
-                        <Link
-                          href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                          aria-label={`Read "${title}"`}
-                        >
-                          Read more &rarr;
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </article>
-              </li>
-            )
-          })}
-        </ul>
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          {siteMetadata.description}
+        </h1>
+        <p className="text-2xl leading-7 text-gray-800 dark:text-gray-400">
+          Exploring the Junior Web Developer's Path
+        </p>
+        <p className="text-sm leading-7 text-gray-500 dark:text-gray-400">
+          by {siteMetadata.author}
+        </p>
       </div>
-      {posts.length > MAX_DISPLAY && (
-        <div className="flex justify-end text-base font-medium leading-6">
-          <Link
-            href="/blog"
-            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label="All posts"
-          >
-            All Posts &rarr;
-          </Link>
-        </div>
-      )}
-    </>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8">
+        {posts.slice(0, MAX_DISPLAY).map((post) => {
+          const { slug, date, title, summary, tags } = post
+          return (
+            <div key={slug} className="bg-white dark:bg-gray-800 rounded-lg p-6 shadow-md">
+              <div className="text-gray-500 mb-2">
+                <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+              </div>
+              <h2 className="text-2xl font-bold text-gray-900 dark:text-gray-100 mb-4">
+                <Link href={`/blog/${slug}`} className="hover:underline">
+                  {title}
+                </Link>
+              </h2>
+              <div className="mb-4">
+                {tags.map((tag) => (
+                  <Tag key={tag} text={tag} />
+                ))}
+              </div>
+              <p className="text-gray-700 dark:text-gray-300">{summary}</p>
+              <div className="mt-4">
+                <Link
+                  href={`/blog/${slug}`}
+                  className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                >
+                  Read more &rarr;
+                </Link>
+              </div>
+            </div>
+          )
+        })}
+        {posts.length > MAX_DISPLAY && (
+          <div className="col-span-full flex justify-center mt-8">
+            <Link
+              href="/blog"
+              className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            >
+              View All Posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -8,32 +8,34 @@ import SearchButton from './SearchButton'
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between py-10">
-      <div>
-        <Link href="/" aria-label={siteMetadata.headerTitle}>
-          <div className="flex items-center justify-between">
+    <header className="py-4">
+      <div className="container mx-auto flex items-center justify-between">
+       <Link href="/" aria-label={siteMetadata.headerTitle}>
+          <div className="flex items-center">
             <div className="mr-3">
               <Logo />
             </div>
             {siteMetadata.headerTitle}
           </div>
         </Link>
-      </div>
-      <div className="flex items-center leading-5 space-x-4 sm:space-x-6">
-        {headerNavLinks
-          .filter((link) => link.href !== '/')
-          .map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className="hidden sm:block font-medium text-gray-900 dark:text-gray-100"
-            >
-              {link.title}
-            </Link>
-          ))}
-        <SearchButton />
-        <ThemeSwitch />
-        <MobileNav />
+        <nav className="hidden md:flex items-center space-x-6 mr-36">
+          {headerNavLinks
+            .filter((link) => link.href !== '/')
+            .map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="hover:text-gray-300 text-gray-900 dark:text-gray-100 transition duration-300"
+              >
+                {link.title}
+              </Link>
+            ))}
+        </nav>
+        <div className="flex items-center space-x-4">
+          <SearchButton />
+          <ThemeSwitch />
+          <MobileNav />
+        </div>
       </div>
     </header>
   )
